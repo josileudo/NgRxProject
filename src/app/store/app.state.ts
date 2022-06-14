19d44a798456ac6dc0369720f6b96b22db47fd93
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2022-06-14 09:17:38 -0300

    :hammer: Add new action

--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,16 +1,21 @@
 import { createAction, createReducer, on, props } from "@ngrx/store";
+import { ITodo } from '../core/model/todo';
 
 export interface IAppState {
   counter: number;
+  todos: ITodo[]
 }
 
 export const appInitialState: IAppState = {
-  counter: 0
+  counter: 0,
+  todos: []
 }
 
 export const incrementCounter = createAction('[App] Counter increase');
 export const decrementCounter = createAction('[App] Counter decrease');
 export const inputCounter = createAction('[App] Counter input', props<{payload: number}>());
+
+export const setTodo= createAction('[App] Define Todos', props<{payload:ITodo[]}>());
 
 export const appReducer = createReducer(
   appInitialState,
@@ -32,6 +37,13 @@ export const appReducer = createReducer(
     state = {
       ...state,
       counter: payload,
+    }
+    return state;
+  }),
+  on(setTodo, (state: IAppState,  { payload } ) => {
+    state = {
+      ...state,
+      todos: payload
     }
     return state;
   })
