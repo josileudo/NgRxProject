7a4e7379fd4cac4d59720846caf6cf4cb2e94b01
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2022-06-16 10:46:20 -0300

    :hammer: Configure new API call method >> This method is responsible for calling the API only once whole in the application

--- src/app/store/todo-effect.service.ts
+++ src/app/store/todo-effect.service.ts
@@ -1,33 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-
-import { loadingTodo, successTodoLoading, IAppState, setTodo } from './app.state';
-import { map, of, switchMap, tap, withLatestFrom } from 'rxjs';
-import { ITodo } from '../core/model/todo';
+import { EMPTY, map, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Store } from '@ngrx/store';
 
+import {
+  loadingTodo,
+  successTodoLoading,
+  IAppState,
+  setTodo,
+} from './app.state';
+import { ITodo } from '../core/model/todo';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoEffectService {
-
   constructor(
     private http: HttpClient,
     private action$: Actions,
     private store: Store<{ app: IAppState }>
-  ) { }
+  ) {}
 
-  loadTodo = createEffect(
-    () => this.action$.pipe(
+  loadTodo = createEffect(() =>
+    this.action$.pipe(
       ofType(loadingTodo),
-      withLatestFrom(this.store.select('app')
-        .pipe(map((app: IAppState) => app.todos))),
-      switchMap(() => {
-        return this.http.get<ITodo[]>('https://jsonplaceholder.typicode.com/todos')
-      }),
-      tap((todos: ITodo[]) => this.store.dispatch(setTodo({ payload: todos }))),
-      map(() => successTodoLoading())
+      withLatestFrom(
+        this.store.select('app').pipe(map((app: IAppState) => app.todos))
+      ),
+      switchMap(([action, todos]) => {
+        if (!todos.length) {
+          return this.http
+            .get<ITodo[]>('https://jsonplaceholder.typicode.com/todos')
+            .pipe(
+              tap((todos: ITodo[]) =>
+                this.store.dispatch(setTodo({ payload: todos }))
+              ),
+              map(() => successTodoLoading())
+            );
+        }
+        return EMPTY;
+      })
     )
-  )
+  );
 }
